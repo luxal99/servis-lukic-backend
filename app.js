c87365b2dece66c7996835cb2c0eb37c6e2c50bf
Aleksa <lukicaleksa04@gmail.com>
2020-06-12 00:38:04 +0200

    Napisan servis za brisanje kategorija

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require('express');
 const app = express();
 const user = require('./routes/users');
 const admin = require('./routes/admin');
-
 const bodyParser = require('body-parser');
+
 
 app.use('/user',user);
 app.use('/admin',admin);
